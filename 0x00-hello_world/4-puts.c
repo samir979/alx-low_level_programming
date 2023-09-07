60b0f6e1784da58cf91a1aab8db1c5e231c25c90
samir979 <samirbabiker74@gmail.com>
2023-09-07 06:16:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main print statement
- * return 0
+ * main - print statement
+ * Return: 0 if exit probsbly 
  */
-
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
