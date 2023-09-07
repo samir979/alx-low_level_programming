c6a96e139cdd4f583dc6e529047c75202b986644
samir979 <samirbabiker74@gmail.com>
2023-09-07 06:27:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print statement
+ * Return: 0 if exit probably
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
