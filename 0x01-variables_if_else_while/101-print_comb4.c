701dcf2a07ad72d0aefd8f8b97ec5dbd7384ee15
samir979 <samirbabiker74@gmail.com>
2023-09-08 11:58:42 -0700

    ./101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print three digits
+ * Return: 0 if exit prbably
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+	int l;
+
+	for (l = 0 ; l != 8 ; l++)
+	{
+		for (n = l + 1 ; n != 9 ; n++)
+		{
+			m = n + 1;
+			do {
+				putchar('0' + l);
+				putchar('0' + n);
+				putchar('0' + m);
+				if (l != 7)
+				{
+					putchar(',');
+					putchar(32);
+				}
+				m++;
+			}while (m != 10);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
