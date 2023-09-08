d0327cb8d79e74bb18af39659fc1fc3a7ed32806
samir979 <samirbabiker74@gmail.com>
2023-09-08 10:15:08 -0700

    ./6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,10 +9,10 @@ int main(void)
 {
         int n;
 
-        for (n = 0 ; n < 10 ; n++)
-        {
-                putchar(n + '0');
-        }
-        putchar('\n');
-        return (0);
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
+	return (0);
 }
