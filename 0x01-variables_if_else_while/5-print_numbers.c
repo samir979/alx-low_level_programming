bc473721357a4d5f98b0078c7afeb64446d5c954
samir979 <samirbabiker74@gmail.com>
2023-09-08 10:02:51 -0700

    ./5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ * Return: 0 if exit probably
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		printf("%d", n);
+	}
+	printf ("\n");
+	return (0);
+}
