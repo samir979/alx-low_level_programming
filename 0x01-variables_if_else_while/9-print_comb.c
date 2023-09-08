ee9b5fbd89eb2a8bd785c939b3e82ad89520bb0b
samir979 <samirbabiker74@gmail.com>
2023-09-08 10:34:34 -0700

    ./9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print single number
+ * Return: 0 if exit probably
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(n + '0');
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
