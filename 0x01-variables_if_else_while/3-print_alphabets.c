d3a2a9cea94807b3a097d128a974dc1f15ac4c2a
samir979 <samirbabiker74@gmail.com>
2023-09-08 04:12:54 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print small and capital letters
+ * Return: 0 if exit probably
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	for (c= 'A' ; c <= 'Z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
