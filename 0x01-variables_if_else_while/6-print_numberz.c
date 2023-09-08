5926b440020a25c018e4fe8ea40c17d24d716f61
samir979 <samirbabiker74@gmail.com>
2023-09-08 10:11:15 -0700

    ./6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ * Return: 0 if exit probably
+ */
+
+int main(void)
+{
+        int n;
+
+        for (n = 0 ; n < 10 ; n++)
+        {
+                putchar(n + '0');
+        }
+        putchar('\n');
+        return (0);
+}
