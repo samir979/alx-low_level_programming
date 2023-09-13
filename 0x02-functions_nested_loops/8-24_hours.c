31d622a1bf9b13b2fba00ee36be07a67b947b75a
samir979 <samirbabiker74@gmail.com>
2023-09-13 01:49:32 -0700

    8-24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - prints 24 hour in timer
+ */
+void jack_bauer(void)
+{
+	int n, m;
+
+	for (n = 0; n < 24; n++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(n / 10 + 48);
+			_putchar(n % 10 + 48);
+			_putchar(':');
+			_putchar(m / 10 + 48);
+                        _putchar(m % 10 + 48);
+                        _putchar('\n');
+		}
+	}
+}
