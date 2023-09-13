799bc6b1bca090e7fcb6ed940456fdf60e0180f3
samir979 <samirbabiker74@gmail.com>
2023-09-13 10:24:23 -0700

    101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/** sumr - returns the sum of numbers
+ * @st: first number
+ * @last: last number
+ * @step: numer of steps
+ * Return: sum
+ */
+int sumr(int st, int last, int step)
+{
+	int sum = 0, i;
+
+	for (i = st; i < last; i += step)
+	{
+		sum += i;
+	}
+	return (sum);
+}
+/**
+ * main - prints the sum of all numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int total;
+
+	total = sumr(3, 1024, 3) + sumr(5, 1024, 5);
+	_putchar(total);
+	_putchar('\n');
+	return (0);
+}
