e48db21d992a47af6cc1b3fc1b07228b0b312d3e
samir979 <samirbabiker74@gmail.com>
2023-09-14 02:17:23 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - function
+ *
+ * Description: multiply two integrs
+ * @n: first number
+ * @m: second number
+ * Return: the result
+ */
+
+int mul(int n, int m)
+{
+	return (n * m);
+}
