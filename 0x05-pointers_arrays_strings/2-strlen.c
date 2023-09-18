a9db6201412f5b820718e5ed81c5bfc72a6ad818
samir979 <samirbabiker74@gmail.com>
2023-09-18 02:41:07 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - count the length of tring
+ * @s: string s
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*s)
+	{
+		len += 1;
+		s += 1;
+	}
+	return (len);
+}
