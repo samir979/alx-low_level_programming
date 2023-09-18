13d7297a922601cd2aae4f0fa8f0ee19b144a9e3
samir979 <samirbabiker74@gmail.com>
2023-09-18 04:26:50 -0700

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _strlen - count length of strength
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * rev_string - reverse string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int len = _strlen(s);
+	int counter = 0;
+	char tmp;
+
+	while (counter < len /2)
+	{
+		tmp = *(s + counter);
+		*(s + counter) = *(s + len - 1 - counter);
+		*(s + len - 1 - counter) = tmp;
+		counter++;
+	}
+}
