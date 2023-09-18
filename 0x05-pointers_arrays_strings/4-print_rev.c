545f7ea1cb4575cb314d1d6ffe482987245a360e
samir979 <samirbabiker74@gmail.com>
2023-09-18 03:07:40 -0700

    4-print_revers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strlen - count the length of string
+ * @s: string
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*s)
+		s++;
+	return (s - p);
+}
+/**
+ * print_rev - print string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int size = _strlen(s);
+
+	while(size--)
+		_putchar(*(s + size));
+	_putchar('\n');
+}
+	
