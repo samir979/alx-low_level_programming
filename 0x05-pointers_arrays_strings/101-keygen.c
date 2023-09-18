2dc546dfb2d12ab41c592b158b7ad0c7eabfe57e
samir979 <samirbabiker74@gmail.com>
2023-09-18 11:39:53 -0700

    101-keygon

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * generate random valid passowrd
+ * Return: 0 if exit ok
+ */
+int main (void)
+{
+	srand(time(0));
+	int num = rand();
+	printf("%i\n", num);
+	return (0);
+}
