3f9bd528f4460ffe33f340ace49d1c6d42fb2f57
samir979 <samirbabiker74@gmail.com>
2023-09-18 04:46:33 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strlen - count string length
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+        int len = 0;
+
+        while (*s)
+        {
+                s++;
+                len++;
+        }
+        return (len);
+}
+
+/**
+ * puts_half - print the second half of string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int len, i, half;
+
+	len = _strlen(str);
+	half = (len % 2 == 0) ? len / 2 : (len - 1) / 2 + 1;
+
+	for (i = half; i < len; i++)
+		_putchar(*(str + i));
+	_putchar('\n');
+}
