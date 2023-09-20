9333030d2299d8143b7b02aa6a67d749d0c080fc
samir979 <samirbabiker74@gmail.com>
2023-09-20 13:21:00 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: size of second string
+ * Return: 0 if exit ok
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int lenD, lenS;
+
+	lenD = 0;
+	lenS = 0;
+
+	while (*(dest + lenD) != '\0')
+		lenD++;
+	while (*(src + lenS) != '\0' && lenD < 97 && lenS < n)
+	{
+		*(dest + lenD) = *(src + lenS);
+		lenD++;
+		lenS++;
+	}
+	*(dest + lenD) = '\0';
+	return (dest);
+}
