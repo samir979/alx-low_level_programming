231dbce5f143be2a6cb246e043616add611b451d
samir979 <samirbabiker74@gmail.com>
2023-09-21 03:08:27 -0700

    100-rotat

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * root13 - encode string using rot13
+ * @s: pointer to string
+ * Return: pointer to encoded s
+ */
+char *rot13(char *s)
+{
+	int scount, rotate;
+
+	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+	       	'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+	       	'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+	
+	for (scount = 0; s[scount] != '\0'; scount++)
+	{
+		for (rotate = 0; rotate < 53; rotate++)
+		{
+			if (r1[rotate] == s[scount])
+			{
+				s[scount] = r2[rotate];
+				break;
+			}
+		}
+	}
+	return (s);
+}
