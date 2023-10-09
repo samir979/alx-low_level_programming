1536d4a885771039e4ec799ea67254e8b9d62245
samir979 <samirbabiker74@gmail.com>
2023-10-08 23:44:56 -0700

    100-realloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
